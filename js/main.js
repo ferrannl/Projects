cfe36ea48285a45ada2fed317db81d48e5364b55
ferrannl <37398317+ferrannl@users.noreply.github.com>
2025-12-31 22:40:12 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -746,6 +746,9 @@ async function loadProjects() {
     if (repo.archived || repo.fork) return false;
     const name = (repo.name || "").toLowerCase();
     if (name === "projects") return false;
+    if (name.includes("Distance")) return false;
+    if (name.includes("Study")) return false;
+    if (name.includes("Monsterzoo")) return false;
     if (name.includes("munchkin")) return false;
     if (name.includes("pso") && name.includes("wiiu")) return false;
     return true;
